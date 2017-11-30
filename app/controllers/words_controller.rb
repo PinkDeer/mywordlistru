750870e8c1047e6b600ff2140d6a6cb348269ae5
PinkDeer <bunman@mail.ru>
2017-11-30 03:01:13 +0300

    Added Word contriller and views

---
+++ app/controllers/words_controller.rb
@@ -0,0 +1,50 @@
+class WordsController < ApplicationController
+
+  before_action :find_word, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @words = Word.all
+  end
+
+  def show
+  end
+
+  def new
+    @word = Word.new
+  end
+
+  def create
+    @word = Word.new(word_params)
+    if @word.save
+     redirect_to @word
+    else :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @word.update_attributes(word_params)
+      redirect_to @word
+    else
+      reder :edit
+    end
+  end
+
+  def destroy
+    @word.destroy
+    redirect_to words_path
+  end
+
+  private
+
+  def find_word
+    @word = Word.find(params[:id])
+  end
+
+  def word_params
+    params.require(:word).permit(:title, :translation)
+  end
+
+end
