4ea607d8b15ac306ad5728fed0c38fef97f2784d
PinkDeer <bunman@mail.ru>
2017-12-05 23:14:06 +0300

    Added validates format for title

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,7 @@
 class Word < ApplicationRecord
 
   validates :title, :translation, presence: true
+  validates_format_of :title, with: /[a-zA-Z]/i, on: :create
 
   before_save :capitalize_fields
 
