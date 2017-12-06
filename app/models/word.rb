154b07568cceca3362034b8366d242eb1af4fc74
PinkDeer <bunman@mail.ru>
2017-12-06 11:11:29 +0300

    Added validates for uniqueness

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,8 +1,9 @@
 class Word < ApplicationRecord
 
+  validates :title, uniqueness: true
   validates :title, :translation, presence: true
   validates_format_of :title, with: /[a-zA-Z]/i, on: :create
-
+  
   before_save :capitalize_fields
 
   def capitalize_fields
