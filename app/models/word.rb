ac23ad4e283a7b2ec06c68fd2b741dd27ffbe6e5
PinkDeer <bunman@mail.ru>
2017-12-06 11:17:06 +0300

    Fix

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,7 +3,7 @@ class Word < ApplicationRecord
   validates :title, uniqueness: true
   validates :title, :translation, presence: true
   validates_format_of :title, with: /[a-zA-Z]/i, on: :create
-  
+
   before_save :capitalize_fields
 
   def capitalize_fields
